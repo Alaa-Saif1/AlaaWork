f9d07876219a2b7e4839f2f469df1126350d4ca2
Alaa-Saif1 <alaasif1999@gmail.com>
2023-05-09 11:42:08 +0400

    Second commit

---
+++ Day To Wor.py
@@ -0,0 +1,28 @@
+# See The Diffrent resulet when we write the equation with/ without pracet:
+b = 2
+r = 50
+n = 1
+
+res1 = b*((1+r/100)**n)
+print("res1=" ,res1)
+
+res2 = b*1+r/100**n
+print("res2=", res2)
+
+# How to use the Opreater:
+#1) 
+baisas = 1729
+omr = baisas // 1000
+baisas = baisas % 1000
+print ("I HAVE", omr, "OR AND", baisas, "baisas")
+
+#2)
+price = 4.35
+quan = 100
+total = price * quan
+print(round(total))
+
+#3) Print the result with two number of digit:
+x = 434.99999
+x = int(x * 100) / 100.0
+print(x)
